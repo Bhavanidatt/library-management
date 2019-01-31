c608b70ed86efa42b8dcff056385c36e1851f814
Parvindar Singh <parvindar.email@gmail.com>
2019-01-31 19:14:50 +0530

    add login and signup functionality using mysql database, database name='library_management', table name='user'

--- Library Management System/LogIn.vb
+++ Library Management System/LogIn.vb
@@ -1,11 +1,67 @@
+Imports MySql.Data.MySqlClient
+
 Public Class LoginForm
+    Dim con As MySqlConnection = New MySqlConnection("server=localhost; user id=root; password=root; database=library_management")
+    'A SET OF COMMAND IN MYSQL
+    Dim cmd As New MySqlCommand
+    'SET A CLASS THAT SERVES AS THE BRIDGE BETWEEN A DATASET AND DATABASE FOR SAVING AND RETRIEVING DATA.
+    Dim da As New MySqlDataAdapter
+    'SET A CLASS THAT CONSISTS SPECIFIC TABLE IN THE DATABASE
+    Dim dt As New DataTable
+    Dim sqlQuery As String
+    Dim result As Integer
+    Dim logined As Boolean = False
 
-	Private Sub LoginButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LoginButton.Click
-		Me.Close()
-	End Sub
+    ' TODO: Insert code to perform custom authentication using the provided username and password 
+    ' (See https://go.microsoft.com/fwlink/?LinkId=35339).  
+    ' The custom principal can then be attached to the current thread's principal as follows: 
+    '     My.User.CurrentPrincipal = CustomPrincipal
+    ' where CustomPrincipal is the IPrincipal implementation used to perform authentication. 
+    ' Subsequently, My.User will return identity information encapsulated in the CustomPrincipal object
+    ' such as the username, display name, etc.
 
-	Private Sub CancelButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CancelButton.Click
-		Me.Close()
-	End Sub
 
+
+    Private Sub UsernameTextBox_TextChanged(sender As Object, e As EventArgs) Handles UsernameTextBox.TextChanged
+
+    End Sub
+
+
+    Private Sub login(ByVal sqlQuery As String)
+        Try
+            con.Open()
+            With cmd
+                .Connection = con
+                .CommandText = sqlQuery
+            End With
+            'FILLING THE DATA IN A SPICIFIC TABLE OF THE DATABASE
+            da.SelectCommand = cmd
+            dt = New DataTable
+            da.Fill(dt)
+            'DECLARING AN INTEGER TO SET THE MAXROWS OF THE TABLE
+            Dim maxrow As Integer = dt.Rows.Count
+            'CHECKING IF THE DATA IS EXIST IN THE ROW OF THE TABLE
+            If maxrow > 0 Then
+                MsgBox("Welcome " & dt.Rows(0).Item(1))
+                logined = True
+            Else
+                MsgBox("Account does not exist.")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        con.Close()
+    End Sub
+
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+        sqlQuery = "SELECT * FROM user WHERE email ='" & UsernameTextBox.Text & "' AND pass = '" & PasswordTextBox.Text & "'"
+        login(sqlQuery)
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub CancelButton_Click(sender As Object, e As EventArgs) Handles CancelBtn.Click
+        Me.Close()
+    End Sub
 End Class
--- Library Management System/SignUp.vb
+++ Library Management System/SignUp.vb
@@ -1,26 +1,76 @@
+Imports MySql.Data.MySqlClient
+
 Public Class SignUpForm
 
-	Private Sub SignupButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SignupButton.Click
-		Me.Close()
-	End Sub
+    Dim con As MySqlConnection = New MySqlConnection("server=localhost; user id=root; password=root; database=library_management")
+    'A SET OF COMMAND IN MYSQL
+    Dim cmd As New MySqlCommand
+    'SET A CLASS THAT SERVES AS THE BRIDGE BETWEEN A DATASET AND DATABASE FOR SAVING AND RETRIEVING DATA.
+    Dim da As New MySqlDataAdapter
+    'SET A CLASS THAT CONSISTS SPECIFIC TABLE IN THE DATABASE
+    Dim dt As New DataTable
+    Dim sqlQuery As String
+    Dim result As Integer
+    Dim usertype As String
+    Dim sex As Char
 
-	Private Sub CancelButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CancelButton.Click
-		Me.Close()
-	End Sub
 
-	Private Sub DropDownButton_Click(sender As Object, e As EventArgs) Handles DropDownButton.Click
-		DropDown.Show(DropDownButton, -151, 25)
-	End Sub
 
-	Private Sub StudentAcc_Click(sender As Object, e As EventArgs) Handles StudentAcc.Click
-		SelectionTextBox.Text = "Student"
-	End Sub
+    Private Sub SignupButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SignupButton.Click
+        sex = "M"
+        If PasswordTextBox.Text <> ConfirmPasswordTextBox.Text Then
+            MessageBox.Show("Passwords do not match!", "Enter password carefully!")
+            Exit Sub
+        End If
+        sqlQuery = "INSERT INTO user( name,email, pass, sex,status)" &
+                    "VALUES ('" & FullnameTextBox.Text & "','" & UsernameTextBox.Text & "','" & PasswordTextBox.Text & "','" & sex & "','" & SelectionTextBox.Text & "')"
+        Register(sqlQuery)
 
-	Private Sub TeacherAcc_Click(sender As Object, e As EventArgs) Handles TeacherAcc.Click
-		SelectionTextBox.Text = "Teacher"
-	End Sub
+        Me.Close()
+    End Sub
 
-	Private Sub AdminAcc_Click(sender As Object, e As EventArgs) Handles AdminAcc.Click
-		SelectionTextBox.Text = "Admin"
-	End Sub
+    Private Sub CancelButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CancelBtn.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DropDownButton_Click(sender As Object, e As EventArgs) Handles DropDownButton.Click
+        DropDown.Show(DropDownButton, -151, 25)
+    End Sub
+
+    Private Sub StudentAcc_Click(sender As Object, e As EventArgs) Handles StudentAcc.Click
+        SelectionTextBox.Text = "Student"
+    End Sub
+
+    Private Sub TeacherAcc_Click(sender As Object, e As EventArgs) Handles TeacherAcc.Click
+        SelectionTextBox.Text = "Teacher"
+    End Sub
+
+    Private Sub AdminAcc_Click(sender As Object, e As EventArgs) Handles AdminAcc.Click
+        SelectionTextBox.Text = "Admin"
+    End Sub
+
+    Private Sub Register(ByVal sqlQuery As String)
+        Try
+            'OPENING THE CONNECTION
+            con.Open()
+            'HOLDS THE DATA TO BE EXECUTED
+            With cmd
+                .Connection = con
+                .CommandText = sqlQuery
+            End With
+            'EXECUTE THE DATA
+            result = cmd.ExecuteNonQuery
+            'CHECKING IF THE DATA HAS BEEN EXECUTED OR NOT
+            If result > 0 Then
+                MsgBox("User has been registered.")
+            Else
+                MsgBox("Failed to register the user.")
+            End If
+            con.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+
 End Class
