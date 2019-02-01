086da6b7e06e6076024d141b95008d2b0433aee9
AvyChanna <30595749+AvyChanna@users.noreply.github.com>
2019-02-01 11:12:26 +0530

    Add SqlInterface Class

---
+++ Library Management System/SqlInterface.vb
@@ -0,0 +1,17 @@
+﻿Public Class SqlInterface
+	Public Shared Sub AuthenticateLogin()
+		' checks if GLogin has correct username/password
+
+		' Use GLogin Here To Get Data
+
+		' Exec queries(scripts) here
+	End Sub
+
+	Public Shared Sub AddSignedUpAccount()
+		' Adds GLogin Data to accounts table
+
+		' Use GLogin Here To Get Data
+
+		' Exec queries(scripts) here
+	End Sub
+End Class
